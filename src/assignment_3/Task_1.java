d489c87ca32fac78f5b1de219b933b1c153bce8f
KleinSamuel <samuel.klein@online.de>
2016-12-16 15:11:14 +0100

    added task 1 assignment 3

---
+++ src/assignment_3/Task_1.java
@@ -0,0 +1,11 @@
+package assignment_3;
+
+import bamfiles.BAMFileReader;
+import bamfiles.Counter;
+
+public class Task_1 {
+
+	public static void main(String[] args) {
+		new BAMFileReader("G:/contextmap.bam", new Counter()).readBAMFile();
+	}
+}
